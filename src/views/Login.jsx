87a28150fd1315f2deb488bcdda8f49d4643aa8a
javiera <jgalazbecerra@gmail.com>
2024-01-02 14:11:41 -0500

    Primer avance estados formularios

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,8 +2,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+import react, {useState} from 'react';
 
 function Login() {
+
+// ESTADOS DEL FORMULARIO
+
+const [email, setEmail] = useState ("");
+const [password, setPassword] = useState ("");
+
+// ESTADO PARA LOS ERRORES
+
+const [error, setError] = useState ("false");
+
+// FUNCIÓN ANTES DE ENVIAR EL FORMULARIO
+
+const validateForm = (e) => {
+e.preventDefault();
+
+//VALIDACIÓN
+
+if (email === "" || password === "") {
+  setError (true);
+  return;
+}
+
+//FORMULARIO ENVIADO CORRECTAMENTE:
+
+setError (false);
+email ("");
+password ("");
+}
 
   return (
     <Form>
@@ -12,8 +43,10 @@ function Login() {
           Email
         </Form.Label>
         <Col sm="10">
-         <Form.Control type="text" placeholder="email" />
+         <Form.Control type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)}
+         value = {email} />
         </Col>
+        
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
@@ -24,7 +57,12 @@ function Login() {
           <Form.Control type="password" placeholder="Password" />
         </Col>
       </Form.Group>
+
+      <Button variant="outline-primary" id="button-addon1">
+          Button
+        </Button>
     </Form>
+    
   );
 }
 
--- src/views/Signin.jsx
+++ src/views/Signin.jsx
@@ -1,50 +1,75 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+
+import react, { useState } from 'react';
 
 function Signin() {
+
+  //ESTADOS DEL FORMULARIO
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [initialBalance, setInitialBalance] = useState("");
+
+  // ESTADO PARA LOS ERRORES
+
+  const [error, setError] = useState("false");
+
+  // FUNCIÓN ANTES DE ENVIAR EL FORMULARIO
+
+  const validateForm = (e) => {
+    e.preventDefault();
+
+    //VALIDACIÓN
+
+    if (name === "" || lastname === "" || email === "" || password === "" || inicialBalance === "") {
+      setError(true);
+      return;
+    }
+
+    //FORMULARIO ENVIADO CORRECTAMENTE:
+
+    setError(false);
+    name("");
+    lastname("");
+    email("");
+    password("");
+    initialBalance("");
+  }
+
   return (
-    
-    <Form>
-    <InputGroup hasValidation>
-      <InputGroup.Text>Name</InputGroup.Text>
-      <Form.Control type="text" required isInvalid />
-      <Form.Control.Feedback type="invalid">
-        Please enter your name.
-      </Form.Control.Feedback>
 
-      <InputGroup.Text>Last name</InputGroup.Text>
-      <Form.Control type="text" required isInvalid />
-      <Form.Control.Feedback type="invalid">
-      Please enter your last name.
-      </Form.Control.Feedback>
-   
-    
-      <InputGroup.Text>Email</InputGroup.Text>
-      <Form.Control type="text" required isInvalid />
-      <Form.Control.Feedback type="invalid">
-        Please enter your email.
-      </Form.Control.Feedback>
-   
+    <Form className='signIn' onSubmit={validateForm}>
+      {error ? <p>Todos los campos son obligatorios</p> : null}
 
-      <InputGroup.Text>Password</InputGroup.Text>
-      <Form.Control type="text" required isInvalid />
-      <Form.Control.Feedback type="invalid">
-        Please enter a password.
-      </Form.Control.Feedback>
+      <InputGroup>
 
+        <InputGroup.Text>Name</InputGroup.Text>
+        <Form.Control type="text" onChange={(e) => setName(e.target.value)} value={name} />
 
-      <InputGroup.Text> initial balance</InputGroup.Text>
-      <Form.Control type="text" required isInvalid />
-      <Form.Control.Feedback type="invalid">
-        Please enter initial balance.
-      </Form.Control.Feedback>
-    </InputGroup>
+        <InputGroup.Text>Last name</InputGroup.Text>
+        <Form.Control type="text" onChange={(e) => setLastname(e.target.value)} value={lastname} />
 
+        <InputGroup.Text>Email</InputGroup.Text>
+        <Form.Control type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
 
+        <InputGroup.Text>Password</InputGroup.Text>
+        <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+
+        <InputGroup.Text> initial balance</InputGroup.Text>
+        <Form.Control type="number" onChange={(e) => setInitialBalance(e.target.value)} value={initialBalance} />
+
+      </InputGroup>
+
+      <Button type='submit'>
+        Submit
+      </Button>
     </Form>
 
-    
+
   );
 }
 
